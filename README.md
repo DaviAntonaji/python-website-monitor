77102f5730b39b2a77e581521d66494cd014fe56
Davi Antonaji <davi.antonajis@gmail.com>
2021-09-29 08:54:09 -0300

    start project

---
+++ README.md
@@ -0,0 +1,19 @@
+#### This project monitors whether website is working 😁
+------------
+##### 👨🏻‍💻 About the project
+###### This project monitors whether the website is working. It uses a no-cache using a ?v=timestamp at the end of the url, and in case of an http code other than 200 it restarts the blog and alerts on the telegram
+
+------------
+##### Installation
+
+- Python recommend version: 3.8.0+
+- Enter the project folder in a terminal and run the command 
+```
+pip3 install -r requirements.txt
+```
+- Change the environment variables collected earlier in the .env file
+- Run the following command to start the application
+```
+python3 app.py
+```
+------------
---
+++ app.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+import random
+import json
+import requests
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+urls = os.getenv("URLS")
+urls = urls.replace("'", '"')
+urls = json.loads(urls)
+
+max_erros = int(os.getenv("ERRORS_TO_ALERT"))
+erros = 0
+while True:
+    now = datetime.now()
+    timestamp = datetime.timestamp(now)
+    posicao = random.randint(0,len(urls)-1)
+    url= urls[posicao] + "?v="+str(int(timestamp))
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            erros += 1
+            if erros == max_erros:
+                TOKEN = os.getenv("TELEGRAM_TOKEN")
+                CHAT_ID= os.getenv("TELEGRAM_CHAT_ID")
+                URL = "https://api.telegram.org/bot"+TOKEN+"/sendMessage"
+                msg = os.getenv("ALERT_MESSAGE")
+                obj = {
+                    "text": msg,
+                    "chat_id": CHAT_ID
+                }
+                x = requests.post(URL, data = obj)
+                erros = 0
+            else:
+                print("Total de Erros: ", erros)
+        else:
+            erros = 0
+            print("Everything is OK :)")
+    except:
+        erros += 1
+        if erros == max_erros:
+            TOKEN = os.getenv("TELEGRAM_TOKEN")
+            CHAT_ID= os.getenv("TELEGRAM_CHAT_ID")
+            URL = "https://api.telegram.org/bot"+TOKEN+"/sendMessage"
+            msg = os.getenv("ALERT_MESSAGE")
+            obj = {
+                "text": msg,
+                "chat_id": CHAT_ID
+            }
+            x = requests.post(URL, data = obj)
+            print("Reiniciando")
+            erros = 0
+        else:
+            print("Total de Erros: ", erros)
+    time.sleep(int(os.getenv("SLEEP_TIME_SECONDS")))
