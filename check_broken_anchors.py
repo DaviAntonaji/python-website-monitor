e32d69e80deae83f51da5ea4b4ee4d18a3b03727
Davi Antonaji <davi@antonaji.com.br>
2023-08-24 15:46:23 -0300

    Fix: new check broken anchors 4

--- check_broken_anchors.py
+++ check_broken_anchors.py
@@ -16,17 +16,16 @@ soup = BeautifulSoup(response.text, "html.parser")
 healthy = True
 for link in soup.find_all("a", href=True):
     link_url = link["href"]
-    if not link_url.startswith("http"):
-        continue
-    try:
-        print("Verifying", link_url)
-        link_response = requests.head(link_url)
-        if link_response.status_code >= 400:
+    if link_url.startswith("http"):
+        try:
+            print("Verifying", link_url)
+            link_response = requests.head(link_url)
+            if link_response.status_code >= 400:
+                healthy = False
+                send_telegram_alert(f"{WEBSITE} Broken link found: {link_url}")
+        except requests.exceptions.RequestException as e:
             healthy = False
-            send_telegram_alert(f"{WEBSITE} Broken link found: {link_url}")
-    except requests.exceptions.RequestException as e:
-        healthy = False
-        send_telegram_alert(f"{WEBSITE} The link couldn't be verified: {link_url}")
+            send_telegram_alert(f"{WEBSITE} The link couldn't be verified: {link_url}")
 
 if not healthy:
     raise Exception("Broken links founded")
