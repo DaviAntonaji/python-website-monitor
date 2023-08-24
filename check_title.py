879cb95aa8b9f73e8d5048f5ebbd5e8eda8d1cd5
Davi Antonaji <davi@antonaji.com.br>
2023-08-24 15:52:54 -0300

    Add check title website

---
+++ check_title.py
@@ -0,0 +1,25 @@
+import requests
+import time
+from datetime import datetime
+from telegram import send_telegram_alert
+from config import WEBSITE
+from bs4 import BeautifulSoup
+
+
+now = datetime.now()
+timestamp = int(datetime.timestamp(now))
+url = f"https://{WEBSITE}?v={timestamp}"
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+title_tag = soup.find("title")
+try:
+    if title_tag:
+        title = title_tag.get_text()
+        if not title:
+            raise Exception(f"{WEBSITE} O site possui uma tag <title> vazia.")
+        elif title != "Davi Antonaji":
+            raise Exception(f"{WEBSITE} O site possui uma tag <title> invalida. ({title})")
+
+except Exception as e:
+    send_telegram_alert(str(e))
